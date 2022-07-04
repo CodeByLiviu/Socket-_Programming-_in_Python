62c6fdff75e0cc54b21d960160061258dbf74b7b
Liviu Moga <107944475+CodeByLiviu@users.noreply.github.com>
2022-07-04 20:26:34 +0300

    Add files via upload

---
+++ server_and_client_port_8000/clientPORT8000.py
@@ -0,0 +1,30 @@
+import socket
+
+HEADER = 64
+PORT = 8000
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+SERVER = "192.168.0.113"
+ADDR = (SERVER, PORT)
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+
+def send(msg):
+    message = msg.encode(FORMAT)
+    print(message)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+    print(client.recv(2048).decode(FORMAT))
+
+
+send("Hello!")
+input()
+send('I AM HERE!')
+input()
+send("I WILL LEAVE!")
+input()
+send(DISCONNECT_MESSAGE)
