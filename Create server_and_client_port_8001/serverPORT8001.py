85bc261efd0e6a75648cc626498bfe4793909655
Liviu Moga <107944475+CodeByLiviu@users.noreply.github.com>
2022-07-04 20:27:55 +0300

    Add files via upload

---
+++ Create server_and_client_port_8001/serverPORT8001.py
@@ -0,0 +1,53 @@
+import socket
+import threading
+
+
+
+HEADER = 64
+PORT = 8001
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
+
+
+def handle_client(conn, addr):
+    '''
+
+    :param conn: connection
+    :param addr: adrress
+    :return:
+    '''
+
+    print(f"[NEW CONNECTION] {addr} connected.")
+    connected = True
+    while connected:
+        msg_length = conn.recv(HEADER).decode(FORMAT)
+        if len(msg_length) > 0:
+            msg_length = len(msg_length)
+            msg = conn.recv(msg_length).decode(FORMAT)
+            if msg == DISCONNECT_MESSAGE:
+                connected = False
+
+            print(f"[{addr}] {msg}")
+            conn.send("Msg received".encode(FORMAT))
+    conn.close()
+
+def start():
+    '''
+    This metod is used to allow the server to listen for and accept new connections.
+    :return:
+    '''
+    server.listen()
+    print(f'[LISTENING] server is listening on {SERVER}')
+    while True:
+        conn, addr = server.accept()
+        thread = threading.Thread(target = handle_client, args = (conn, addr))
+        thread.start()
+        print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
+
+print("[STARTING] server is starting...")
+start()
